931988969774db2ac640ab3ae2899dd28291d320
Erik Scholten <tazz74@gmail.com>
2024-10-18 15:36:04 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.72.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "Web-server" {
+  ami = "ami-0084a47cc718c111a"
+  instance_type = "t2.small"
+  
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash
+  apt -y update
+  apt -y install apache2
+  ufw allow 'Apache'
+  echo "<html><h1>webpage 1(I've been provisioned using HasiCorp Terraform!)</h1></html>" > /var/www/html/index.html
+  systemctl start apache2
+  EOF
+  
+  tags = {
+    Name = "WebServer"
+  }
+}
