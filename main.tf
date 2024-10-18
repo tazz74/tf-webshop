263ff617f790948b69a8b13e3824c34e88f673d3
Erik <aws@scholten.team>
2024-10-18 17:51:37 +0200

    LB added

--- main.tf
+++ main.tf
@@ -11,6 +11,25 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
+resource "aws_security_group" "webshop_ext_access" {
+    name        = "webshop_ext_access"
+    description = "Allow incoming HTTP/HTTPS connections"
+    
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "Webshop" {
   ami = "ami-0084a47cc718c111a"
   instance_type = "t2.micro"
@@ -32,4 +51,53 @@ resource "aws_instance" "Webshop" {
   tags = {
     Name = var.instance_name
   }
+}
+
+resource "aws_lb" "webshop-lb" {
+    name            = "webshop-alb"
+    internal        = false
+    ip_address_type     = "ipv4"
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.web-server.id]
+    subnets = [
+                data.aws_subnet.subnet1.id,
+                data.aws_subnet.subnet2.id,
+                data.aws_subnet.subnet3.id
+                ]
+    tags = {
+        Name = "webshop-alb"
+        Environment = var.environment
+    }
+}
+
+resource "aws_lb_target_group" "target-group" {
+    health_check {
+        interval            = 10
+        path                = "/"
+        protocol            = "HTTPS"
+        timeout             = 5
+        healthy_threshold   = 5
+        unhealthy_threshold = 2
+    }
+    name          = "webshop-tg"
+    port          = 443
+    protocol      = "HTTPS"
+    target_type   = "instance"
+    vpc_id = var.vpc_id
+}
+
+resource "aws_lb_listener" "alb-listener" {
+    load_balancer_arn          = aws_lb.application-lb.arn
+    port                       = 443
+    protocol                   = "HTTPS"
+    default_action {
+        target_group_arn         = aws_lb_target_group.target-group.arn
+        type                     = "forward"
+    }
+}
+
+resource "aws_lb_target_group_attachment" "ec2_attach" {
+    count = length(aws_instance.Webshop)
+    target_group_arn = aws_lb_target_group.target-group.arn
+    target_id        = aws_instance.web-server[count.index].id
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
 # Input variable definitions
 
+variable "vpc_id" {
+  description = "Where to deploy"
+  type        = string
+  default     = "vpc-0ea1fb5e7fb5fd725"
+}
+
 variable "instance_name" {
   description = "Name of VM"
   type        = string
@@ -22,4 +28,10 @@ variable "security_group_id" {
   description = "ID of security group to use"
   type        = string
   default     = "sg-0e3d71fa803d5f91e"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "development"
 }
