c745ae61b1cf93bcd4e8412a0152766f2cbc2384
Erik <aws@scholten.team>
2024-10-18 19:35:01 +0200

    OK

--- main.tf
+++ main.tf
@@ -79,9 +79,9 @@ resource "aws_lb" "webshop-lb" {
     
     value = [for s in data.aws_subnet.public : s.id]
     subnets = [
-                data.aws_subnet.public.id,
-                data.aws_subnet.public.id,
-                data.aws_subnet.public.id
+                data.aws_subnet.public[each.key].id,
+                data.aws_subnet.public[each.key].id,
+                data.aws_subnet.public[each.key].id
                 ]
     
     tags = {
