be4adfc00f0d8d9dd595f933b1de70680df166ff
Erik Scholten <tazz74@gmail.com>
2024-10-21 21:50:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
 terraform {
   required_providers {
     aws = {
@@ -12,8 +15,8 @@ provider "aws" {
 }
 
 # Create a Security Group for an EC2 instance
-resource "aws_security_group" "webshop_sg" {
-  name = "webshop_sg"
+resource "aws_security_group" "${var.environment}-webshop_sg" {
+  name = "${var.environment}-webshop_sg"
   
   ingress {
     from_port	  = 8080
@@ -28,7 +31,7 @@ resource "aws_instance" "Webshop" {
   instance_type = "t2.micro"
   count = var.num_nodes
   #subnet_id = var.subnet_id
-  vpc_security_group_ids  = ["${aws_security_group.webshop_sg.id}"]
+  vpc_security_group_ids  = ["${aws_security_group.${var.environment}-webshop_sg.id}"]
   associate_public_ip_address = true
 
   user_data = <<-EOF
