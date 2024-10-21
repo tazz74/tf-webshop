1d7510cdcb21b0548f7aa3f712f376046fa8949b
Erik Scholten <tazz74@gmail.com>
2024-10-21 21:29:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,24 +14,34 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
+# Create a Security Group for an EC2 instance
+resource "aws_security_group" "webshop_sg" {
+  name = "cli_demo_sg"
+  
+  ingress {
+    from_port	  = 8080
+    to_port	  = 8080
+    protocol	  = "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "Webshop" {
-  ami = "ami-0084a47cc718c111a"
+  ami = "ami-0745b7d4092315796"
   instance_type = "t2.micro"
   count = var.num_nodes
   subnet_id = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
+  vpc_security_group_ids  = ["${aws_security_group.cli_demo_sg.id}"]
   associate_public_ip_address = true
 
   user_data = <<-EOF
-  #!/bin/bash
-  apt -y update
-  apt -y install apache2
-  ufw allow 'Apache'
-  echo "<html><h1>I've been provisioned using HasiCorp Terraform Cloud!</h1></html>" > /var/www/html/index.html
-  systemctl start apache2
-  EOF
+      #!/bin/bash
+      echo "<hmtl><body><h1>I've been provisioned using HasiCorp Terraform Cloud!</h1></body></html>" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
   
   tags = {
     Name = var.instance_name
+    Environment = var.environment
   }
 }
